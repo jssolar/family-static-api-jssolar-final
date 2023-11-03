42323a2fc2a860ecdf913d9949d4bd503b4b9924
Juan Solar <juansolarsolar@yahoo.com>
2023-11-03 01:53:01 -0300

    actualizacion

--- src/app.py
+++ src/app.py
@@ -5,38 +5,37 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
-#from models import Person
+from flask import Flask, jsonify, request
+from datastructures import Family
+
+
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-CORS(app)
-
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
-
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+jackson_family = Family('Jackson')
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
+    return jsonify(jackson_family.get_all_members()), 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({'error': 'Member not found'}), 404
 
+@app.route('/member', methods=['POST'])
+def add_member():
+    data = request.json
+    if data:
+        jackson_family.add_member(data)
+        return jsonify({'status': 'Member added'}), 200
+    return jsonify({'error': 'Invalid data provided'}), 400
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    jackson_family.delete_member(member_id)
+    return jsonify({'done': True}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
